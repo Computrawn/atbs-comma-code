2fe5359ee563e9364645c1343f7d1586b52aba3a
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-05 13:12:07 -0500

    Refactor complete.

--- comma_code.py
+++ comma_code.py
@@ -13,39 +13,30 @@ logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
 def eggs(spam):
-    """
-    Print list contents with commas between items and 'and' at penultimate position if
-    length of list greater than 1. Otherwise, print zero index or inform user of index error.
-    """
-    try:
-        if len(spam) == 1:
-            print(spam[0])
-        else:
-            spam.insert(-1, "and")
-            for bacon in spam[:-2]:
-                print(f"{bacon}, ", end="")
-            print(f"{spam[-2]} {spam[-1]}")
-    except IndexError:
-        print("List is empty.")
+    """Returns string of contents from list with commas between items and 'and' at penultimate position if
+    length of list greater than 1. Otherwise, return zero index."""
+
+    if len(spam) == 1:
+        bacon = spam[0]
+    else:
+        spam_string = [str(bacon) for bacon in spam]
+        bacon = f"{', '.join(spam_string[:-1])}, and {spam_string[-1]}"
+    return bacon
 
 
-spam = ["Alice", "Bob", 3.14159256, "apples", "oranges", 42, "cats", "dogs"]
+spam_1 = ["Alice", "Bob", 3.14159256, "apples", "oranges", 42, "cats", "dogs"]
 # Outputs 'Alice, Bob, 3.14159256, apples, oranges, 42, cats, and dogs'
-spam1 = ["Bob", {"age": 42, "height": "6'9"}]
-# Outputs 'Bob, and {'age': 42, 'height': "6'9"}'
-spam2 = []
-# Outputs 'List is empty.'
-spam3 = ["Alice"]
+spam_2 = ["Alice"]
 # Outputs 'Alice'
-spam4 = [1]
-# Outputs '1'
-
-spam_list = [spam, spam1, spam2, spam3, spam4]
+spam_3 = []
+# Outputs 'List is empty.'
 
 
 def main():
-    for item in spam_list:
-        eggs(item)
+    try:
+        print(eggs(spam_1))
+    except IndexError:
+        print("List is empty")
 
 
 if __name__ == "__main__":
