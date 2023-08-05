c6ee67fa1db6b6c70b1295caecd593235af34fc7
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-05 13:29:24 -0500

    Fix docstrings, move vriables and main to top.

--- comma_code.py
+++ comma_code.py
@@ -11,19 +11,6 @@ logging.basicConfig(
 )
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
-
-def eggs(spam):
-    """Returns string of contents from list with commas between items and 'and' at penultimate position if
-    length of list greater than 1. Otherwise, return zero index."""
-
-    if len(spam) == 1:
-        bacon = spam[0]
-    else:
-        spam_string = [str(bacon) for bacon in spam]
-        bacon = f"{', '.join(spam_string[:-1])}, and {spam_string[-1]}"
-    return bacon
-
-
 spam_1 = ["Alice", "Bob", 3.14159256, "apples", "oranges", 42, "cats", "dogs"]
 # Outputs 'Alice, Bob, 3.14159256, apples, oranges, 42, cats, and dogs'
 spam_2 = ["Alice"]
@@ -33,10 +20,23 @@ spam_3 = []
 
 
 def main():
+    """Call eggs on argument with exception for empty list."""
     try:
         print(eggs(spam_1))
     except IndexError:
         print("List is empty")
+
+
+def eggs(spam):
+    """Takes in list and returns string of contents with commas between items and 'and'
+    at penultimate position if length greater than 1. Otherwise, return zero index."""
+
+    if len(spam) == 1:
+        bacon = spam[0]
+    else:
+        spam_string = [str(bacon) for bacon in spam]
+        bacon = f"{', '.join(spam_string[:-1])}, and {spam_string[-1]}"
+    return bacon
 
 
 if __name__ == "__main__":
